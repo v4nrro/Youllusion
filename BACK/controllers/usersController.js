2400fd73e28d15bbe10edcc7f93dc74c5bf48ed3
v4nrro <ivannavarro0105@gmail.com>
2025-04-13 11:47:18 +0200

    feat: Added functional put and delete routes to Users

--- BACK/controllers/usersController.js
+++ BACK/controllers/usersController.js
@@ -1,4 +1,5 @@
 const Users = require('../models/Users');
+const bcrypt = require('bcrypt');
 
 const getAllUsers = async (req, res) => {
     try {
@@ -47,23 +48,112 @@ const getLoggedUser = async (req, res) => {
     }
 }
 
-const putByMe = async (req, res) => {
+const putCredentials = async (req, res) => {
+    try {
+        const { username, email } = req.body;
 
+        if( !username || !email ) {
+           return res.status(400).json({ message: "Missing required fields" });
+        }
+
+        const updatedUser = await Users.findByIdAndUpdate(
+            req.user.userId,
+            { username, email },
+            { new: true, runValidators: true }
+        );
+
+        if(!updatedUser){
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json({ message: "Credentials updated succesfully", updatedUser });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+const putAvatar = async (req, res) => {
+    try {
+        if(!req.file) {
+            return res.status(400).json({ message: "Missing required fields" });
+        }
+
+        const updatedUser = await Users.findByIdAndUpdate(
+            req.user.userId,
+            { avatar: req.file.path },
+            { new: true, runValidators: true }
+        );
+
+        if(!updatedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json({ message: "Avatar updated succesfully", updatedUser });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+const putPassword = async (req, res) => {
+    try {
+        const { previousPassword, newPassword, confirmNewPassword } = req.body
+
+        if(newPassword != confirmNewPassword) {
+            return res.status(400).json({ message: "Passwords do not match" });
+        }
+
+        const user = await Users.findById(req.user.userId);
+        
+        if(!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const isMatch = await bcrypt.compare(previousPassword, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Incorrect previous password" });
+        }
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+        const updatedUser = await Users.findByIdAndUpdate(
+            req.user.userId,
+            { password: hashedPassword },
+            { new: true, runValidators: true }
+        );
+
+        res.status(200).json({ message: "Password updated succesfully", updatedUser});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
 
 const deleteByMe = async (req, res) => {
+    try {
+        await Users.findByIdAndDelete(req.user.userId);
 
+        res.status(200).json({ message: "Account deleted succesfully"});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
 
 const deleteByAdmin = async (req, res) => {
+    try {
+        await Users.findByIdAndDelete(req.params.id);
 
+        res.status(200).json({ message: "Account deleted succesfully"});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
 
 module.exports = {
     getAllUsers,
     getUserById,
     getLoggedUser,
-    putByMe,
+    putCredentials,
+    putPassword,
+    putAvatar,
     deleteByMe,
     deleteByAdmin,
 };
--- BACK/routes/users.js
+++ BACK/routes/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
-const { getLoggedUser, getAllUsers, getUserById, deleteByAdmin, deleteByMe, putByMe } = require("../controllers/usersController.js");
+const { getLoggedUser, getAllUsers, getUserById, deleteByAdmin, deleteByMe, putCredentials, putAvatar, putPassword } = require("../controllers/usersController.js");
 const { authenticate, role } = require("../middleware/auth.js");
+const { imageUpload } = require('../utils/multer.js');
 
 const router = express.Router();
 
@@ -9,7 +10,10 @@ router.get('', authenticate, role(['admin']), getAllUsers);
 router.get('/me', authenticate, getLoggedUser);
 router.get('/:id', getUserById);
 
-router.put('/me/:id', authenticate, putByMe);
-router.delete('/me/:id', authenticate, deleteByMe);
-router.delete('/:id', authenticate, role(['admin']), deleteByAdmin)
+router.put('/me/credentials', authenticate, putCredentials);
+router.put('/me/avatar', authenticate, imageUpload.single('avatar'), putAvatar);
+router.put('/me/password', authenticate, putPassword);
+
+router.delete('/me', authenticate, deleteByMe);
+router.delete('/:id', authenticate, role(['admin']), deleteByAdmin);
 module.exports = router;
