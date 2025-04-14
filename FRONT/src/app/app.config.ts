619c7c13b8feef2c01a493a011187d27dbbfe013
v4nrro <ivannavarro0105@gmail.com>
2025-04-14 12:23:07 +0200

    feat: Prepairing project and adding navbar

--- FRONT/src/app/app.config.ts
+++ FRONT/src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
+import {
+  PreloadAllModules,
+  provideRouter,
+  withComponentInputBinding,
+  withPreloading,
+} from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes)]
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes, withComponentInputBinding(), withPreloading(PreloadAllModules)),
+    provideHttpClient(/* withInterceptors([baseUrlInterceptor, authInterceptor]) */),
+  ],
 };
--- FRONT/src/app/app.routes.ts
+++ FRONT/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {
+    //     path: 'events',
+    //     loadChildren: () => import('./events/event.routes').then(m => m.eventsRoutes),
+    //     data: { animation: 'eventsPage' }
+    // },
+    // {
+    //     path: 'auth',
+    //     loadChildren: () => import('./auth/auth.routes').then(m => m.authRoutes),
+    // },
+    // {
+    //     path: 'profile',
+    //     loadChildren: () => import('./profile/profile.routes').then(m => m.profileRoutes),
+    // },
+    { path: '', redirectTo: '/auth/login',  pathMatch: 'full' },
+    { path: '**', redirectTo: '/auth/login' },
+];
