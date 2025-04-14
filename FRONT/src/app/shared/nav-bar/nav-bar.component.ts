b3693d7d7fb583132d34e4a756b5341b21b1cb09
v4nrro <ivannavarro0105@gmail.com>
2025-04-14 13:44:29 +0200

    feat: Creating and styling navbar

--- FRONT/src/app/shared/nav-bar/nav-bar.component.ts
+++ FRONT/src/app/shared/nav-bar/nav-bar.component.ts
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-
+    isMenuCollapsed = false;
+  
+    navItems = [
+      { icon: 'bi bi-house-door-fill text-white', label: 'Home' },
+      { icon: 'bi bi-compass text-white', label: 'Explore' },
+      { icon: 'bi bi-collection-play text-white', label: 'Subscriptions' },
+      { icon: 'bi bi-archive text-white', label: 'Library' },
+      { icon: 'bi bi-clock-history text-white', label: 'History' },
+      { icon: 'bi bi-play-btn text-white', label: 'Your videos' },
+      { icon: 'bi bi-clock text-white', label: 'Watch later' },
+      { icon: 'bi bi-hand-thumbs-up text-white', label: 'Liked videos' }
+    ];
+  
+    toggleSidebar() {
+      this.isMenuCollapsed = !this.isMenuCollapsed;
+    }
+    
+    search(term: string) {
+      console.log('Searching for:', term);
+      // Implement your search logic here
+    }
 }
