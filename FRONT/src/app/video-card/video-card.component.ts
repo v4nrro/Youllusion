15cd2d19ea590fe48a874d722eb15e973063d103
v4nrro <ivannavarro0105@gmail.com>
2025-04-14 17:24:48 +0200

    feat: Created HomePageComponent and VideoCardComponent

---
+++ FRONT/src/app/video-card/video-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'video-card',
+  imports: [],
+  templateUrl: './video-card.component.html',
+  styleUrl: './video-card.component.css'
+})
+export class VideoCardComponent {
+
+}
