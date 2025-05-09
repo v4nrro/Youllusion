8989a31df397b707e06e074c2c80e01de2c99aad
v4nrro <ivannavarro0105@gmail.com>
2025-05-09 11:49:26 +0200

    feat: Adding login and logout guards

--- FRONT/src/app/auth/auth.routes.ts
+++ FRONT/src/app/auth/auth.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
+import { logoutActivateGuard } from '../shared/guards/logout-activate.guard';
 
 export const authRoutes: Routes = [
     {
         path: 'login',
         loadComponent: () => import('./login/login.component').then(m => m.LoginComponent),
+        canActivate: [logoutActivateGuard],
     },
     {
         path: 'register',
         loadComponent: () => import('./register/register.component').then(m => m.RegisterComponent),
+        canActivate: [logoutActivateGuard],
     },
     // {
     //     path: 'profile',
---
+++ FRONT/src/app/shared/guards/login-activate.guard.ts
@@ -0,0 +1,17 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../../auth/service/auth.service';
+import { map } from 'rxjs';
+
+export const loginActivateGuard: CanActivateFn = (route, state) => {
+    const authService = inject(AuthService);
+    const router = inject(Router);
+  
+    return authService.isLogged()
+    .pipe(map((result) => {
+        if(!result){
+            return router.createUrlTree(['/auth/login']);
+        }
+        return true;
+    }))
+};
