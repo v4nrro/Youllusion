240206057282aa9a4b1f69894b6539928a80d5c6
v4nrro <ivannavarro0105@gmail.com>
2025-05-13 13:06:53 +0200

    feat: Making token not expiring because I'm not implementing refresh yet

--- BACK/controllers/authController.js
+++ BACK/controllers/authController.js
@@ -58,9 +58,7 @@ const login = async (req, res) => {
             role: user.role
         };
 
-        const token = jwt.sign(payload, process.env.JWT_SECRET, {
-            expiresIn: "1h",
-        });
+        const token = jwt.sign(payload, process.env.JWT_SECRET);
 
         res.status(200).json({
             message: "Login successful",
