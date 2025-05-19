e66dcbc20b13a4dabd5526a9979bfbb0013cad43
v4nrro <ivannavarro0105@gmail.com>
2025-05-19 13:34:51 +0200

    feat: Adding myVideosComponent

---
+++ FRONT/src/app/profile/my-videos/my-videos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-videos',
+  imports: [],
+  templateUrl: './my-videos.component.html',
+  styleUrl: './my-videos.component.css'
+})
+export class MyVideosComponent {
+
+}
